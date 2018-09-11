a2f9d253a60c12d9e84412624b190a6acfba73a1
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-09-11 23:46:19 +0800

    AC leet P107

---
+++ juneyuan/2018/09/2018_09_11.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)   return result;
+
+        Deque<List<Integer>> stack = new ArrayDeque<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int qSize = q.size();
+            for (int i = 0; i < qSize; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                // push child nodes into queue
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            stack.push(list);
+        }
+
+        while (!stack.isEmpty()) {
+            result.add(stack.pop());
+        }
+
+        return result;
+    }
+}
