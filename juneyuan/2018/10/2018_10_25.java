71e52296db149b898f6846b82b8a90139c97e9b7
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-10-25 23:46:28 +0800

    AC leet P46

---
+++ juneyuan/2018/10/2018_10_25.java
@@ -0,0 +1,61 @@
+class Solution {
+    public List<List<Integer>> permute(int[] A) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (A == null || A.length == 0)     return ret;
+
+        List<Integer> list = new ArrayList<>();
+        dfs(A, list, ret);
+
+        return ret;
+    }
+    
+    private void dfs(int[] A, List<Integer> list, List<List<Integer>> ret) {
+        if (list.size() == A.length) {
+            ret.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            if (list.contains(A[i]))    continue;
+            list.add(A[i]);
+            dfs(A, list, ret);
+            list.remove(list.size() - 1);
+        }
+    }
+    
+}
+
+// Sln2
+class Solution {
+    public List<List<Integer>> permute(int[] A) {
+        List<List<Integer>> ret = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        
+        if (A == null) {
+            return ret;
+        } else {
+            for (int item : A) {
+                list.add(item);
+            }
+        }
+        
+        if (A.length <= 1) {
+            ret.add(list);
+            return ret;
+        }
+        
+        for (int i = 0; i < A.length; i++) {
+            int[] B = new int[A.length - 1];
+            System.arraycopy(A, 0, B, 0, i);
+            System.arraycopy(A, i + 1, B, i, A.length - i - 1);
+            
+            List<List<Integer>> tmpRet = permute(B);
+            for (List<Integer> tmp : tmpRet) {
+                tmp.add(A[i]);
+                ret.add(tmp);
+            }
+        }
+        
+        return ret;
+    }
+}
