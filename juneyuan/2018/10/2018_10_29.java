76d619b3a0add32812da654bfe3a2f5e7f0e121d
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-10-31 02:20:18 +0800

    AC leet P151

--- juneyuan/2018/10/2018_10_29.java
+++ juneyuan/2018/10/2018_10_29.java
@@ -4,8 +4,10 @@ class Solution {
 
         int len = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                if (len > 0) return len;
+            if (s.charAt(i) == ' ' && len == 0) {
+                continue;
+            } else if (s.charAt(i) == ' ' && len > 0) {
+                return len;
             } else {
                 len++;
             }
--- juneyuan/README.md
+++ juneyuan/README.md
@@ -2,5 +2,6 @@ start at 5 Sep, 2018
 
 Exceptions:
 
+2018.10.28 Affected by the messy room
 2018.10.23 Shame, but feel too tired and sleepy...
 
