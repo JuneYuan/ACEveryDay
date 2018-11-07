1e6b814b4226d7372ae976602797534d9ce232a3
yuanzhongyan <yuanzhongyan@xiaomi.com>
2018-11-07 22:54:24 +0800

    AC leet P60

---
+++ juneyuan/2018/11/2018_11_07.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        
+        int fact = 1;
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+            fact *= i;
+        }
+        
+        for (int i = n; i >= 1; i--) {
+            fact /= i;
+            int rank = (k-1) / fact;
+            k = (k-1) % fact + 1;
+            sb.append(nums.get(rank));
+            nums.remove(rank);
+        }
+        
+        return sb.toString();
+    }
+}
--- juneyuan/README.md
+++ juneyuan/README.md
@@ -2,6 +2,7 @@ start at 5 Sep, 2018
 
 Exceptions:
 
+2018.11.4~6
 2018.10.28 Affected by the messy room
 2018.10.23 Shame, but feel too tired and sleepy...
 
